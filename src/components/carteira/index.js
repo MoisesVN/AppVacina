74e5889a0ba4e7e295afed5b7411560a9eef8c3e
LopesGP <lopesjuanpablo5@gmail.com>
2024-06-02 22:28:32 -0300

    fazendo o básico da Carteira aberta
    fiz esse protótipo da tela de Carteira aberta, logo mais vamos melhora-la.

--- src/components/carteira/index.js
+++ src/components/carteira/index.js
@@ -10,7 +10,34 @@ export default function Info() {
                 <TouchableOpacity onPress={() => navigation.navigate("Home")} style={styles.botao}>
                     <Image source={require("../imagens/kisspng-brand-logo-black-and-white-triangle-arrow-symbol-5a76c6d1df2c13.8388288415177335859141 (1).png")} />
                 </TouchableOpacity>
-                <Text style={styles.Text}>Carteira Aberta</Text>
+
+
+                <View style={styles.Divdecima}>
+                    <View style={styles.nomeIdade}>
+                        <Text style={styles.Txt}>nome: João</Text>
+                        <Text>idade: 3 anos</Text>
+                    </View>
+                    <View style={styles.imgdePerfil}>
+                        <Text>Imagem aqui!</Text>
+                    </View>
+                </View>
+                <View style={styles.DivdoMeio}>
+                <Text style={styles.textMeio}>
+                    adic. foto
+                </Text>
+                </View>
+
+                <View style={styles.DivdoMeio}>
+                <Text style={styles.textMeio}>
+                    alergias
+                </Text>
+                </View>
+            <View style={styles.divconteiner}>
+                <View style={styles.DivdeBaixo}>
+                    <Text>Próxima vacina</Text>
+                </View>
+            </View> 
+
             </View>
         </View>
     )
--- src/components/carteira/style.js
+++ src/components/carteira/style.js
@@ -2,28 +2,79 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
 
-    botao:{
-        marginLeft:20,
-        marginTop:40,
-        padding:10,
-        backgroundColor:'#392DE9',
-        borderRadius:8,
+    botao: {
+        marginLeft: 20,
+        marginTop: 40,
+        padding: 10,
+        backgroundColor: '#392DE9',
+        borderRadius: 8,
     },
-    Tela:{
-        alignItems:'flex-start',
+    Tela: {
+        alignItems: 'flex-start',
     },
-    Tela1:{
-        flex:1,
-        backgroundColor:'#1E90FF'
+    Tela1: {
+        flex: 1,
+        backgroundColor: "#FFFFFF",
     },
-    Text:{marginTop:30,
-        marginLeft:20,
-        fontSize: 20,
+
+    Divdecima: {
+        marginTop: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+
     },
-    Text2:{
-        color:'#FFFFFF',
-        fontSize:20
+    imgdePerfil: {
+        width: 140,
+        height: 100,
+        display: 'flex',
+        justifyContent: 'center',
+        margin: 10,
+        marginRight: 10,
+        backgroundColor: "#ECECEC",
+        borderRadius: 18,
+        alignItems: 'center'
+    },
+    nomeIdade: {
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: "#ECECEC",
+        width: 230,
+        height: 100,
+        margin: 10,
+        borderRadius: 18,
+    },
+    DivdoMeio: {
+
+        margin: 10,
+        padding: 10,
+        justifyContent: 'center',
+        borderRadius: 18,
+        marginLeft: 20,
+        width: 370,
+        height: 100,
+        backgroundColor: '#392DE9'
+
+    },
+    textMeio: {
+        color: 'white'
+    },
+    DivdeBaixo: {
+        backgroundColor: "#ECECEC",
+        width: 370,
+        height: 200,
+        margin: 10,
+        marginLeft: 20,
+        borderRadius: 18,
+        padding: 10,
+    },
+
+    divconteiner: {
+
+        display: 'flex',
+        alignItems: 'center',
     }
+
 
 });
 
