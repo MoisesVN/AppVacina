6efa8a0ad5c426ea35c6ab0e553600256300ac62
Moisés Vila Nova <143229789+MoisesVN@users.noreply.github.com>
2024-06-06 10:58:09 -0300

    Update GetCarteiraVacina.java
    Adicionado um get para retornar um foto pelo id da carteira de vacina

--- Back-End/Api/teste-api/src/main/java/br/com/projeto/testeapi/controle/CarteiraVacina/GetCarteiraVacina.java
+++ Back-End/Api/teste-api/src/main/java/br/com/projeto/testeapi/controle/CarteiraVacina/GetCarteiraVacina.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,7 +57,19 @@ public class GetCarteiraVacina {
         }
         return ResponseEntity.ok(carteiraVacina);
     } */
-
+    @GetMapping("/foto/{id_carteira}")
+    public ResponseEntity<byte[]> fotoCarteiraID(@PathVariable Long id_carteira) {
+    Optional<CarteiraVacina> carteiraVacinaOptional = carteira_Vacina_Reposirotorio.findById(id_carteira);
+    if (carteiraVacinaOptional.isPresent()) {
+        CarteiraVacina carteiraVacina = carteiraVacinaOptional.get();
+        // Retornar a imagem como um array de bytes com o tipo de conteúdo adequado
+        return ResponseEntity.ok()
+            .contentType(MediaType.IMAGE_JPEG) // ou MediaType.IMAGE_PNG, dependendo do tipo de imagem
+            .body(carteiraVacina.getFoto());
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+}
 
 
 }
